660586d25e76042c779ab8eb566bbeb0464a5004
dc376 <dc376@njit.edu>
2017-12-21 10:39:03 -0500

    Create pickaSport.php

---
+++ pickaSport.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Kehoe's Bros Betting</title> 
+<style>
+body {margin: 0;}
+#css1{
+  background-color: black;
+  background-position: right bottom;
+  background-repeat: repeat;
+  padding: .5px;
+}
+.topnav{
+    overflow: hidden;
+    background-color:black;
+    }
+.topnav a{
+    float: center;
+    display: inline;
+    color: white;
+    text-align: center;
+    padding: 5px 10px;
+    text-decoration: none;
+    font-size: 10px;
+    }
+.topnav a:hover{
+    background-color: lightblue;
+    color: white;
+}
+.topnav a.active{
+    background-color: #ee2211;
+    color: red;
+}   
+.box1{
+    position: relative;
+    margin: auto;
+    float: center;
+    width: 400px;
+    font-family:"georgia";
+    height: 100px;
+    background-color: pink;
+    text-align: left;
+    padding-left: .5cm;
+    padding-top: .05cm;
+    padding-bottom: .8cm;
+}
+</style>
+</head>
+
+<!--<body>
+// <?php 
+// 
+// include ("currentBalance.php"); 
+// 
+// echo $userBalanceResult;
+// 
+// ?>
+</body>-->
+
+
+<head>
+<div id = "css1">
+<center>
+<h3><font face="georgia" size="5" color="white">Kehoe's Bros Betting</font></h3>
+    <div class="topnav" id="firstTopNav">
+        <a href="pickaSport.php">HOME</a>
+        <a href="deposit.php">DEPOSIT</a>
+        <a href="about.php">ABOUT</a>
+    </div>
+</center>
+</div>
+</body>
+</head>
+
+<style>
+    { margin: 0; padding: 0; }
+    html {
+        background-color: white;
+        -webkit-background-size: cover;
+        -moz-background-size: cover;
+        -o-background-size: cover;
+        background-size: cover;
+    }
+</style>
+
+
+<body>
+<p><center><b><font face="georgia" size="20" color="black">CHOOSE A SPORT:</font></b></center></p>
+
+<center>
+    <a href="NBAhomepage.php">
+    <img src ="http://www.gameongrafix.com/sc_images/products/NBA%20Jam%20sideart-sca1-1000.jpg" style="border:0px;margin:0px;height:350px;width:200;">
+    </a>	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="homepage.php">
+    <img src="https://images.complex.com/complex/image/upload/c_limit,w_680/fl_lossy,pg_1,q_auto/dtcgpflfr7yxaucc7sfz.jpg" style="border:0px;margin:0px;clear:both;height:350px; width:200;">
+    </a>
+</center>
+</body>
